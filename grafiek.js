ecf37addf7ff601405627f1139472e89ea5056bd
themilanguy1 <milan.gupta@ymail.com>
2018-06-26 11:12:49 +0200

    chart alcoholconsumptie klaar

--- grafiek.js
+++ grafiek.js
@@ -45,11 +45,13 @@ $(document).ready(function(){
 			var jaar = [];
             var bier = [];
             var wijn = [];
+            var gedistilleerd = [];
 
 			for(var i in data) {
 				jaar.push(data[i].jaar);
                 bier.push(data[i].bier);
                 wijn.push(data[i].wijn);
+                gedistilleerd.push(data[i].gedistilleerd);
 			}
 
 			var chartdata = {
@@ -57,19 +59,18 @@ $(document).ready(function(){
 				datasets : [
 					{
 						label: "Bier", 
-						backgroundColor: 'rgba(200, 200, 200, 0)',
-						borderColor: 'rgba(200, 200, 200, 0.75)',
-						hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
-						hoverBorderColor: 'rgba(200, 200, 200, 1)',
+                        borderColor: 'rgb(255, 1, 1, 0.7)',
 						data: bier
                     }, 
                     {
 						label: "Wijn", 
-						backgroundColor: 'rgba(255, 200, 200, 0)',
-						borderColor: 'rgba(200, 200, 200, 0.75)',
-						hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
-						hoverBorderColor: 'rgba(200, 200, 200, 1)',
+                        borderColor: 'rgb(43, 255, 1, 0.7)',
 						data: wijn
+                    },
+                    {
+						label: "Gedistilleerd", 
+                        borderColor: 'rgb(90, 1, 255, 0.7)',
+						data: gedistilleerd
                     }
 				]
 			};
